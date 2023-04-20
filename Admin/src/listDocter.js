e22a9497dc9f5d637ea162d60fc438cecf3c3796
thanawat1303 <fquarius13@gmail.com>
2023-04-21 01:03:34 +0700

    set list page

--- Admin/src/listDocter.js
+++ Admin/src/listDocter.js
@@ -1,6 +1,9 @@
 import React , {Component} from "react";
 import {clientMo}  from "./assets/js/moduleClient";
 
+import './assets/style/List.scss'
+
+
 export default class List extends Component {
     
     constructor(){
@@ -15,10 +18,17 @@ export default class List extends Component {
         if (this.props.status == 0) window.history.replaceState({} , null , '/list' )
         else if(this.props.status == 1) window.history.pushState({}, null , '/list')
 
+        console.log(JSON.parse(this.props.list))
         this.setState({
             body : JSON.parse(this.props.list).map((listDT , index) =>
-                        <div key={index}>
-                            <p>{listDT['id_docter']}</p>
+                        <div key={index} className="container-docter">
+                            <img className="img-docter" src={(listDT['Image_docter']['data'] != '') ? listDT['Image_docter']['data'] : '/doctor-svgrepo-com.svg'}></img>
+                            <div className="content-detail">
+                                <div className="detail-private">
+                                    <span>{(listDT['Fullname_docter']) ? listDT['Fullname_docter'] : 'ไม่ระบุชื่อ' }</span>
+                                    <span>{listDT['id_docter']}</span>
+                                </div>
+                            </div>
                         </div>
                     ) // use map is create element object
         })

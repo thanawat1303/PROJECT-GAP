fd4cca2f3b9eb25e743c7b26719bd4856975ab49
Pinchattaporn <pin161144@gmail.com>
2023-05-05 22:06:17 +0700

    set Richmenu

--- API/api_line_gap.js
+++ API/api_line_gap.js
@@ -8,53 +8,119 @@ const config = {
 }
 const richmenu = new Line.Client(config)
 
-const createRichLogin = () => {
-  const jsonLogin = 
-  {
-    "size": {
-      "width": 2500,
-      "height": 843
-    },
-    "selected": true,
-    "name": "Login",
-    "chatBarText": "เข้าสู่ระบบ",
-    "areas": [
-      {
-        "bounds": {
-          "x": 0,
-          "y": 0,
-          "width": 1250,
-          "height": 843
-        },
-        "action": {
-          "type": "uri",
-          "uri": "line:https://liff.line.me/1661049098-A9PON7LB"
-        }
+const RichMenu = {
+  createRichLogin : () => {
+    const jsonLogin = 
+    {
+      "size": {
+        "width": 2500,
+        "height": 843
       },
-      {
-        "bounds": {
-          "x": 1252,
-          "y": 0,
-          "width": 1250,
-          "height": 843
+      "selected": true,
+      "name": "Login",
+      "chatBarText": "เข้าสู่ระบบ",
+      "areas": [
+        {
+          "bounds": {
+            "x": 0,
+            "y": 0,
+            "width": 1250,
+            "height": 843
+          },
+          "action": {
+            "type": "uri",
+            "uri": "line:https://liff.line.me/1661049098-A9PON7LB"
+          }
         },
-        "action": {
-          "type": "uri",
-          "uri": "line:https://liff.line.me/1661049098-dorebKYg"
+        {
+          "bounds": {
+            "x": 1252,
+            "y": 0,
+            "width": 1250,
+            "height": 843
+          },
+          "action": {
+            "type": "uri",
+            "uri": "line:https://liff.line.me/1661049098-dorebKYg"
+          }
         }
-      }
-    ]
-  }
-  
-  richmenu.createRichMenu(jsonLogin).then((RichID)=>{
-    let pathImage = "assets/login.png"
-    richmenu.setRichMenuImage(RichID , fs.readFileSync(pathImage) , "image/png").then(()=>{
-      richmenu.setDefaultRichMenu(RichID).then(()=>console.log(`Rich menu ID : ${RichID} is Run`))
+      ]
+    }
+    
+    richmenu.createRichMenu(jsonLogin).then((RichID)=>{
+      let pathImage = "assets/login.png"
+      richmenu.setRichMenuImage(RichID , fs.readFileSync(pathImage) , "image/png").then(()=>{
+        richmenu.setDefaultRichMenu(RichID).then(()=>console.log(`Rich menu ID : ${RichID} is Run`))
+      })
     })
-  })
+
+  },
+  
+  DeleteRichMenu : {
+    All : () => {
+      richmenu.getRichMenuList().then((val) => {
+        if (val.length == 0) {
+          console.log("No RichMenu")
+          return 0
+        }
+        val.map(item => richmenu.deleteRichMenu(item.richMenuId).then(()=>console.log("Delete All complete")))
+      })
+    },
+    someone : (RichMenuID) => {
+      richmenu.deleteRichMenu(RichMenuID).then(()=>console.log("Delete Complete"))
+    }
+  }
+
 }
 
-createRichLogin()
+RichMenu.createRichLogin()
+
+// const createRichLogin = () => {
+  // const jsonLogin = 
+  // {
+  //   "size": {
+  //     "width": 2500,
+  //     "height": 843
+  //   },
+  //   "selected": true,
+  //   "name": "Login",
+  //   "chatBarText": "เข้าสู่ระบบ",
+  //   "areas": [
+  //     {
+  //       "bounds": {
+  //         "x": 0,
+  //         "y": 0,
+  //         "width": 1250,
+  //         "height": 843
+  //       },
+  //       "action": {
+  //         "type": "uri",
+  //         "uri": "line:https://liff.line.me/1661049098-A9PON7LB"
+  //       }
+  //     },
+  //     {
+  //       "bounds": {
+  //         "x": 1252,
+  //         "y": 0,
+  //         "width": 1250,
+  //         "height": 843
+  //       },
+  //       "action": {
+  //         "type": "uri",
+  //         "uri": "line:https://liff.line.me/1661049098-dorebKYg"
+  //       }
+  //     }
+  //   ]
+  // }
+  
+  // richmenu.createRichMenu(jsonLogin).then((RichID)=>{
+  //   let pathImage = "assets/login.png"
+  //   richmenu.setRichMenuImage(RichID , fs.readFileSync(pathImage) , "image/png").then(()=>{
+  //     richmenu.setDefaultRichMenu(RichID).then(()=>console.log(`Rich menu ID : ${RichID} is Run`))
+  //   })
+  // })
+// }
+
 // const Create = () => {
 //   const signinRich = {
 //     size: {
@@ -87,12 +153,6 @@ createRichLogin()
 //     });
 //   });
 // };
-
-// const Delete = () => {
-//   client.getRichMenuList().then((val) => {
-//     val.map(item => client.deleteRichMenu(item.richMenuId))
-//   })
-// }
 
 // const GetAll = () => {
 //   client.getRichMenuList().then(val => {

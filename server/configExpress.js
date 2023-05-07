574804e7dcee64f614286d0e7d311c4a17ddc680
Pinchattaporn <pin161144@gmail.com>
2023-05-07 12:49:35 +0700

    set server

--- server/configExpress.js
+++ server/configExpress.js
@@ -43,5 +43,6 @@ app.use(express.static('src/assets/style'))
 app.use(express.static('src/assets/font'))
 app.use(express.static('src/assets/img'))
 app.use(express.static('src/assets/icon'))
+app.use(express.static('public'))
 
 module.exports = app

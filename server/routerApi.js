360cac5c17bab9a069e3cfe56cec7ee5ed897d74
thanawat1303 <fquarius13@gmail.com>
2023-05-08 15:28:57 +0700

    router docter /

--- server/routerApi.js
+++ server/routerApi.js
@@ -1,26 +1,29 @@
 module.exports = router = (app) => {
+    // router admin
     app.get('/admin' , function(req, res) {
         res.sendFile(__dirname.replace('\server' , '/public/index.html'));
     });
-
     app.get('/admin/list' , function(req, res) {
         res.sendFile(__dirname.replace('\server' , '/public/index.html'));
     });
-
     // plus
     app.get('/admin/plus' , function(req, res) {
         res.sendFile(__dirname.replace('\server' , '/public/index.html'));
     });
-
     app.get('/admin/plus/confirm' , function(req, res) {
         res.sendFile(__dirname.replace('\server' , '/public/index.html'));
     });
     // plus
-
     app.get('/admin/docter' , function(req, res) {
         res.sendFile(__dirname.replace('\server' , '/public/index.html'));
     });
 
+    // router docter
+    app.get('/docter' , function(req, res) {
+        res.sendFile(__dirname.replace('\server' , '/public/index.html'));
+    });
+
+    // page error 404
     app.get('/' , function(req, res) {
         res.sendFile(__dirname.replace('\server' , '/public/index404.html'));
     });

09f82a1a73a424171319c500e380960f705a240a
thanawat1303 <fquarius13@gmail.com>
2023-07-11 13:13:38 +0700

    set server ts to js

--- server/appRun.js
+++ server/appRun.js
@@ -1,10 +1,11 @@
 const appConfig = require("./configExpress")
 const fs = require('fs')
 module.exports = function appRun(username , password){
-    console.log("Load Server ...")
+    console.log("Load Server...")
     const UrlApi = fs.readFileSync(__dirname.replace('\server' , "/UrlServer.json"))
     const app = appConfig(username , password , JSON.parse(UrlApi.toString()).url) 
-    app.listen(parseInt(process.env.PORT ?? "80") , "0.0.0.0" , function () {
-        console.log('Start on port '+process.env.PORT+'!\n');
+    const Port = parseInt(process.env.PORT ?? "80")
+    app.listen( Port , "0.0.0.0" , function () {
+        console.log('Start on port '+Port+'!\n');
     });
 }

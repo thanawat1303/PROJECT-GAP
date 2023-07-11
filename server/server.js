5cb21ccbf4cfde92bfe8b7ead29672762a40a657
thanawat1303 <fquarius13@gmail.com>
2023-07-12 00:21:30 +0700

    set server

--- server/server.js
+++ server/server.js
@@ -7,8 +7,8 @@ const db  = require('mysql')
 const appRun = require("./appRun")
 require('dotenv').config().parsed
 
-let username = (process.argv[2] == process.env.BUILD) ? process.argv[3] : process.env.USER_DBDEV ?? ""
-let password = (process.argv[2] == process.env.BUILD) ? process.argv[4] : process.env.PASSWORD_DBDEV ?? ""
+let username = (process.argv[2] == process.env.BUILD) ? process.argv[3] ?? "" : process.env.USER_DBDEV ?? ""
+let password = (process.argv[2] == process.env.BUILD) ? process.argv[4] ?? "" : process.env.PASSWORD_DBDEV ?? ""
 let state = 0
 
 console.log("Read Database...")

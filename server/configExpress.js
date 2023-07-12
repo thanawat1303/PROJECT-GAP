8cc28c47a9f5fac652a2dc9b969d879aa883a9b3
thanawat1303 <fquarius13@gmail.com>
2023-07-13 00:59:53 +0700

    set server

--- server/configExpress.js
+++ server/configExpress.js
@@ -24,12 +24,11 @@ module.exports = function appConfig(username , password , UrlNgrok ) {
     const app = express();
     const upload = multer()
     const server = 
-                // (process.argv[2] == process.env.BUILD) ? https.createServer({
-                //     key: fs.readFileSync(`${__dirname.replace('\server' , 'SSL')}/key.pem`),
-                //     cert: fs.readFileSync(`${__dirname.replace('\server' , 'SSL')}/cert.pem`)
-                // } , app) 
-                // : 
-                http.createServer(app)
+                (process.argv[2] == process.env.BUILD) ? https.createServer({
+                    key: fs.readFileSync(`${__dirname.replace('\server' , 'SSL')}/key.pem`),
+                    cert: fs.readFileSync(`${__dirname.replace('\server' , 'SSL')}/cert.pem`)
+                } , app) 
+                : http.createServer(app)
     // set Server
 
     const listDB = dbpacket.listConfig(username , password)

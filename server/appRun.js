d9b0f88cf0eeb5a2cb5f04684281ddeaeaca63d4
thanawat1303 <fquarius13@gmail.com>
2023-07-15 23:16:01 +0700

    set server

--- server/appRun.js
+++ server/appRun.js
@@ -3,7 +3,8 @@ const fs = require('fs')
 module.exports = function appRun(username , password){
     console.log("Load Server...")
     const UrlApi = fs.readFileSync(__dirname.replace('\server' , "/UrlServer.json"))
-    const app = appConfig(username , password , (process.argv[2] == process.env.BUILD) ? "https://" + process.env.HOST_SERVER + process.env.PORT : JSON.parse(UrlApi.toString()).url) 
+    const UrlServer = "https://" + process.env.HOST_SERVER +":"+process.env.PORT
+    const app = appConfig(username , password , (process.argv[2] == process.env.BUILD) ? UrlServer : JSON.parse(UrlApi.toString()).url) 
     const Port = parseInt(process.env.PORT ?? "80")
     app.listen( Port , "0.0.0.0" , function () {
         console.log('Start on port '+Port+'!\n');

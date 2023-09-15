666f382042cc12a9248356efd2cbc1cc84964f80
thanawat1303 <fquarius13@gmail.com>
2023-09-15 21:46:07 +0700

    set database

--- server/apifunc.js
+++ server/apifunc.js
@@ -9,7 +9,7 @@ const apifunc = {
     return new Promise((resole, reject) => {
       connectDB.connect((err) => {
         if (err) {
-          ErrorDB(connectDB, err, res);
+          // ErrorDB(connectDB, err, res);
           reject("connect");
         }
 
@@ -18,21 +18,23 @@ const apifunc = {
 
         let ORDER = authAccount == "admin" ? "" : authAccount == "acc_doctor" ? "ORDER BY status_delete ASC" : "";
         connectDB.query(
-          `SELECT * FROM ${authAccount} WHERE ${usernameDB}=? AND ${passwordDB}=SHA2( ? , 256) ${ORDER}`,
+          `SELECT * FROM ${authAccount} WHERE ${usernameDB} = ? AND ${passwordDB}=SHA2( ? , 256) ${ORDER}`,
           [username, password],
           (err, result) => {
             if (err) {
-              ErrorDB(connectDB, err, res);
-              reject("query");
-            }
-
-            if (result[0]) {
-              resole({
-                data : result[0],
-                result:"pass"
-              });
-            } else {
+              // ErrorDB(connectDB, err, res);
               reject("not pass");
+            } else {
+              console.log(result)
+
+              if (result[0]) {
+                resole({
+                  data : result[0],
+                  result:"pass"
+                });
+              } else {
+                reject("not pass");
+              }
             }
           }
         );
